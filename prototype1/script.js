42834f33f264e5837e46da5a997174e16f85ff10
qk23kk <>
2026-01-18 18:00:42 -0500

    Added second mesh

--- prototype1/script.js
+++ prototype1/script.js
@@ -9,7 +9,7 @@ const canvas = document.querySelector('.webgl')
 
 // Scene
 const scene = new THREE.Scene()
-scene.background = new THREE.Color('lightBlue')
+scene.background = new THREE.Color('pink')
 // Camera
 const camera = new THREE.PerspectiveCamera(
     75, //field of view
@@ -32,11 +32,18 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 ************/
 
 // testSphere
-const sphereGeometry = new THREE.SphereGeometry(1) //1 is the radius
+const sphereGeometry = new THREE.SphereGeometry()
 const sphereMaterial = new THREE.MeshNormalMaterial()
 const testSphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
 
+// testCapsule
+const capsuleGeometry = new THREE.CapsuleGeometry(0.5, 0.4)
+const capsuleMaterial = new THREE.MeshNormalMaterial()
+const testCapsule = new THREE.Mesh(capsuleGeometry, capsuleMaterial)
+
+
 scene.add(testSphere)
+scene.add(testCapsule)
 
 
 /*******************
@@ -51,6 +58,10 @@ const animation = () => {
 
     // Animate testSphere
     testSphere.position.y = Math.sin(elapsedTime)
+
+    // Animate testCapsule
+    testCapsule.position.x = Math.cos(elapsedTime) * 2
+    testCapsule.position.y = -Math.sin(elapsedTime)
 
     // Renderer
     renderer.render(scene, camera)
